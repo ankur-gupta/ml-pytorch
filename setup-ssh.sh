e2e2a583f00fab9a96720d1184538385ab3e1177
ankur-gupta <7110058+ankur-gupta@users.noreply.github.com>
2023-08-06 19:37:32 -0700

    Fixing permissions for ssh keys

--- setup-ssh.sh
+++ setup-ssh.sh
@@ -4,5 +4,6 @@ if [[ ${PUBLIC_KEY} ]]; then
     mkdir -p ${HOME}/.ssh
     echo ${PUBLIC_KEY} >> ${HOME}/.ssh/authorized_keys
     chmod 700 -R ${HOME}/.ssh
+    chmod 400 ${HOME}/.ssh/authorized_keys
     service ssh start
 fi
