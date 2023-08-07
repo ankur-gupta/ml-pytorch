0ff36508b6731a7bb225c53ea539a6bc12226e6d
ankur-gupta <7110058+ankur-gupta@users.noreply.github.com>
2023-08-07 16:08:38 -0700

    Removing unnecessary file

--- setup-ssh.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ ${PUBLIC_KEY} ]]; then
-    mkdir -p ${HOME}/.ssh
-    echo ${PUBLIC_KEY} >> ${HOME}/.ssh/authorized_keys
-    chmod 700 -R ${HOME}/.ssh
-    chmod 400 ${HOME}/.ssh/authorized_keys
-    service ssh start
-fi
